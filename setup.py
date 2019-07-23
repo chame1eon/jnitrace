4525c89eb9cbdfc59c5a06ac443866d94e5a9877
david <david-1993-j@hotmail.com>
2019-07-23 22:07:59 +0100

    Removed dev file and added resource data into the MANIFEST file. Also bumped the version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='jnitrace',
-    version='1.0.0',
+    version='1.0.3',
     description='A tool for tracing use of the JNI in Android apps',
     long_description=long_description,
     long_description_content_type='text/markdown',
