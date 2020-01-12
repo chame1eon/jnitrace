12bf9d4b7e75cc5f7257d2d54f1e870dc751d76b
david <david-1993-j@hotmail.com>
2020-01-12 14:43:30 +0000

    Bumped version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='jnitrace',
-    version='3.0.1',
+    version='3.0.2',
     description='A tool for tracing use of the JNI in Android apps',
     long_description=long_description,
     long_description_content_type='text/markdown',
